673cb6cf69126645fbd11d489ae51684a37a265d
smk-1991 <samuel.lima1991@gmail.com>
2022-12-10 18:46:24 -0300

    implementhed method find by id in controller

--- src/main/java/com/br/dorotech/app/controllers/ProductController.java
+++ src/main/java/com/br/dorotech/app/controllers/ProductController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +32,13 @@ public class ProductController {
         final ProductDTO createProductDTO = productService.createNewProduct(productDTO);
         return new ResponseEntity<>(createProductDTO, HttpStatus.CREATED);
     }
+
+    @GetMapping(value = "/getProductById/{id}")
+    public ResponseEntity<ProductDTO> getFarmById(@PathVariable Long id){
+        final ProductDTO productDTO = productService.findProductById(id);
+        return new ResponseEntity<>(productDTO, HttpStatus.OK);
+    }
+
+
 
 }
