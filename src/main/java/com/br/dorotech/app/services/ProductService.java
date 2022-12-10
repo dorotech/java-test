43a0f10a8b348aa73bdaeddb98676ba5e06d94f5
smk-1991 <samuel.lima1991@gmail.com>
2022-12-10 17:05:24 -0300

    created service classes for products

---
+++ src/main/java/com/br/dorotech/app/services/ProductService.java
@@ -0,0 +1,7 @@
+package com.br.dorotech.app.services;
+
+public interface ProductService {
+
+
+
+}
---
+++ src/main/java/com/br/dorotech/app/services/impl/ProductServiceImpl.java
@@ -0,0 +1,9 @@
+package com.br.dorotech.app.services.impl;
+
+import com.br.dorotech.app.services.ProductService;
+
+public class ProductServiceImpl implements ProductService {
+
+
+
+}
