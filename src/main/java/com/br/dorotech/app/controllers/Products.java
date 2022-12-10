ea6b1afe4161f7355da44e6907a490908f242720
smk-1991 <samuel.lima1991@gmail.com>
2022-12-10 16:06:51 -0300

    created entity products and also added H2 database dependency

---
+++ src/main/java/com/br/dorotech/app/controllers/Products.java
@@ -0,0 +1,42 @@
+package com.br.dorotech.app.controllers;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Products {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column
+    private Double price;
+
+    @Column
+    private Double amount;
+
+}
