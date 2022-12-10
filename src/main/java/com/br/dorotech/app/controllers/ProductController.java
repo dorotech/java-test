2bd71bcaf374ea689588fc4014735930d2be279f
smk-1991 <samuel.lima1991@gmail.com>
2022-12-10 17:11:47 -0300

    created class product controller

---
+++ src/main/java/com/br/dorotech/app/controllers/ProductController.java
@@ -0,0 +1,12 @@
+package com.br.dorotech.app.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+
+
+
+}
