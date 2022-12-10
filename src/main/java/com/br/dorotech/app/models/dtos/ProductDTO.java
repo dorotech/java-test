284a13b676b695cc0ac2da8f9ade3af319bf4e23
smk-1991 <samuel.lima1991@gmail.com>
2022-12-10 18:22:14 -0300

    refactor changed Product DTO class

--- src/main/java/com/br/dorotech/app/models/dtos/ProductDTO.java
+++ src/main/java/com/br/dorotech/app/models/dtos/ProductDTO.java
@@ -1,18 +1,21 @@
 package com.br.dorotech.app.models.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
 @Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDTO {
 
     private String name;
-
     private String description;
-
     private Double price;
-
     private Double amount;
 
 }
