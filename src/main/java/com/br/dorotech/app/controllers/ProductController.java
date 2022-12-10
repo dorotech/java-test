33e6f3091175efe4e0938ba0d6be290107acbf64
smk-1991 <samuel.lima1991@gmail.com>
2022-12-10 18:12:30 -0300

    implemented get method in product controller

--- src/main/java/com/br/dorotech/app/controllers/ProductController.java
+++ src/main/java/com/br/dorotech/app/controllers/ProductController.java
@@ -1,11 +1,28 @@
 package com.br.dorotech.app.controllers;
 
+import com.br.dorotech.app.models.dtos.ProductDTO;
+import com.br.dorotech.app.services.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping(value = "/getAllProducts")
+    public ResponseEntity<List<ProductDTO>> getAllFarms() {
+        final List<ProductDTO> productDTOS = productService.findAllProducts();
+        return new ResponseEntity<>(productDTOS, HttpStatus.OK);
+    }
 
 
 
