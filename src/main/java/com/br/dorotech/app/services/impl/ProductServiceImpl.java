402b637f269522ecb0058831cc3a80723f81b2dd
smk-1991 <samuel.lima1991@gmail.com>
2022-12-10 17:44:43 -0300

    implemented method create new product in product services

--- src/main/java/com/br/dorotech/app/services/impl/ProductServiceImpl.java
+++ src/main/java/com/br/dorotech/app/services/impl/ProductServiceImpl.java
@@ -1,20 +1,34 @@
 package com.br.dorotech.app.services.impl;
 
 import com.br.dorotech.app.models.dtos.ProductDTO;
+import com.br.dorotech.app.models.entities.Products;
+import com.br.dorotech.app.repositories.ProductRepository;
 import com.br.dorotech.app.services.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
 
+    private final ProductRepository productRepository;
 
     @Override
     public ProductDTO createNewProduct(ProductDTO productDTO) {
-        return null;
+        Products product = new Products();
+        product.setName(productDTO.getName());
+        product.setAmount(productDTO.getAmount());
+        product.setDescription(productDTO.getDescription());
+        product.setPrice(productDTO.getPrice());
+        productRepository.save(product);
+        return productDTO;
     }
 
     @Override
     public ProductDTO findAllProducts() {
+        List<Products> productsList = productRepository.findAll();
         return null;
     }
 
