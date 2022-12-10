cc0d9f02ffe112e499490bc062ce2f9fea32ad5c
smk-1991 <samuel.lima1991@gmail.com>
2022-12-10 17:39:00 -0300

    refactor changed parameter order in JpaRepository extension

--- src/main/java/com/br/dorotech/app/repositories/ProductRepository.java
+++ src/main/java/com/br/dorotech/app/repositories/ProductRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Long, Products> {
+public interface ProductRepository extends JpaRepository<Products, Long> {
 
 
 }
