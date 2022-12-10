229d3093082ba7e6e9bf37c808675d5cfd93ca60
smk-1991 <samuel.lima1991@gmail.com>
2022-12-10 17:28:16 -0300

    created service methods in services and implemented empty methods in service impl

--- src/main/java/com/br/dorotech/app/services/ProductService.java
+++ src/main/java/com/br/dorotech/app/services/ProductService.java
@@ -1,7 +1,17 @@
 package com.br.dorotech.app.services;
 
+import com.br.dorotech.app.models.dtos.ProductDTO;
+
 public interface ProductService {
 
+    ProductDTO createNewProduct(ProductDTO productDTO);
 
+    ProductDTO findAllProducts();
+
+    ProductDTO findProductById(Long id);
+
+    void deleteProduct(Long id);
+
+    void updateProduct(Long id);
 
 }
--- src/main/java/com/br/dorotech/app/services/impl/ProductServiceImpl.java
+++ src/main/java/com/br/dorotech/app/services/impl/ProductServiceImpl.java
@@ -1,9 +1,33 @@
 package com.br.dorotech.app.services.impl;
 
+import com.br.dorotech.app.models.dtos.ProductDTO;
 import com.br.dorotech.app.services.ProductService;
 
 public class ProductServiceImpl implements ProductService {
 
 
+    @Override
+    public ProductDTO createNewProduct(ProductDTO productDTO) {
+        return null;
+    }
 
+    @Override
+    public ProductDTO findAllProducts() {
+        return null;
+    }
+
+    @Override
+    public ProductDTO findProductById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+
+    }
+
+    @Override
+    public void updateProduct(Long id) {
+
+    }
 }
