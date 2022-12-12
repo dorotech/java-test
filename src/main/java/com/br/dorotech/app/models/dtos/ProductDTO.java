f4acbc296f491109b0022754c6d064daaceb8fda
smk-1991 <samuel.lima1991@gmail.com>
2022-12-12 18:22:14 -0300

    refactor removed unused imports

--- src/main/java/com/br/dorotech/app/models/dtos/ProductDTO.java
+++ src/main/java/com/br/dorotech/app/models/dtos/ProductDTO.java
@@ -3,9 +3,7 @@ package com.br.dorotech.app.models.dtos;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Builder
 @Data
